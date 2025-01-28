05fd887559c548b3e68a3ab7818b9631262dbfde
Tiple <tipleshashank@gmail.com>
2025-01-28 13:41:14 -0500

    changes in StudentConfig class made on Main branch

--- src/main/java/com/example/demo/edu_config/StudentConfig.java
+++ src/main/java/com/example/demo/edu_config/StudentConfig.java
@@ -1,22 +1,30 @@
 package com.example.demo.edu_config;
 
+import com.example.demo.edu_class.Student;
+import com.example.demo.edu_repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @Configuration
 public class StudentConfig {
 
-//    @Bean
-//    CommandLineRunner commandLineRunner(StudentRepository repository) {
-//        return args -> {
-//            Student shashank = new Student("Shashank", LocalDate.of(1993, Month.MAY, 07), "shanky@gmail.com"
-//            );
-//
-//            Student tiple = new Student("tiple", LocalDate.of(2000, Month.JANUARY, 01), "tiple@gmail.com"
-//            );
-//
-//            repository.saveAll(
-//                    List.of(shashank, tiple)
-//            );
-//        };
-//    }
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student shashank = new Student("Shashank", LocalDate.of(1993, Month.MAY, 07), "shanky@gmail.com"
+            );
+
+            Student tiple = new Student("tiple", LocalDate.of(2000, Month.JANUARY, 01), "tiple@gmail.com"
+            );
+
+            repository.saveAll(
+                    List.of(shashank, tiple)
+            );
+        };
+    }
 }
