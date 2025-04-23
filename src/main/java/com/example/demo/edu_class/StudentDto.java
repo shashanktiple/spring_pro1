1bfee988a77a223d04d2272b8649e287bf2120cd
Tiple <tipleshashank@gmail.com>
2025-04-23 10:37:50 -0400

    Added a new Dto file for test

---
+++ src/main/java/com/example/demo/edu_class/StudentDto.java
@@ -0,0 +1,53 @@
+package com.example.demo.edu_class;
+
+import java.time.LocalDate;
+
+
+public class StudentDto {
+
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private Integer age;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
